13a157c0733c9abc93fe627f5c48c089ba59fb21
AlAminAshik <106368872+AlAminAshik@users.noreply.github.com>
2025-03-31 01:30:09 +0600

    eid message code added

--- FERD_main/src/Lora_receiver.cpp
+++ FERD_main/src/Lora_receiver.cpp
@@ -1,94 +1,94 @@
-///////////////Connections of lora//////////////////
-// NodeMCU esp8266 Pins                     SX1278 Pins
-// GND                                       GND
-// 3.3V                                      VCC
-// D8                                        NSS
-// D7                                        MOSI
-// D6                                        MISO
-// D5                                        SCK
-// D0                                        RST
-// D2                                        DIO0
-/////////////////////////////////////////////////
+// ///////////////Connections of lora//////////////////
+// // NodeMCU esp8266 Pins                     SX1278 Pins
+// // GND                                       GND
+// // 3.3V                                      VCC
+// // D8                                        NSS
+// // D7                                        MOSI
+// // D6                                        MISO
+// // D5                                        SCK
+// // D0                                        RST
+// // D2                                        DIO0
+// /////////////////////////////////////////////////
 
 
-#include <SPI.h>                // Include the SPI library for SPI communication
-#include <LoRa.h>               // Or include the appropriate library header by sandeep mistry
-#include <Wire.h>               // Include the Wire library for I2C communication
-#include <Adafruit_GFX.h>       // Include the Adafruit GFX library for graphics
-#include <Adafruit_SSD1306.h>   // Include the Adafruit SSD1306 library for OLED display
+// #include <SPI.h>                // Include the SPI library for SPI communication
+// #include <LoRa.h>               // Or include the appropriate library header by sandeep mistry
+// #include <Wire.h>               // Include the Wire library for I2C communication
+// #include <Adafruit_GFX.h>       // Include the Adafruit GFX library for graphics
+// #include <Adafruit_SSD1306.h>   // Include the Adafruit SSD1306 library for OLED display
 
-int counter = 0;
+// int counter = 0;
  
-// Define LoRa module pins (adjust as needed)
-#define SS 15  // LoRa module's SPI Slave Select (SS) pin
-#define RST 16  // LoRa module's Reset pin
-#define DI0 11  // LoRa module's Interrupt pin (DIO0)
+// // Define LoRa module pins (adjust as needed)
+// #define SS 15  // LoRa module's SPI Slave Select (SS) pin
+// #define RST 16  // LoRa module's Reset pin
+// #define DI0 11  // LoRa module's Interrupt pin (DIO0)
 
-//setup the display
-#define i2c_Address 0x3C //initialize with the I2C addr 0x3C Typically eBay OLED's
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET      -1  // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+// //setup the display
+// #define i2c_Address 0x3C //initialize with the I2C addr 0x3C Typically eBay OLED's
+// #define SCREEN_WIDTH 128 // OLED display width, in pixels
+// #define SCREEN_HEIGHT 64 // OLED display height, in pixels
+// // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// #define OLED_RESET      -1  // Reset pin # (or -1 if sharing Arduino reset pin)
+// Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
-void setup() {
-  Serial.begin(115200); // Initialize serial communication for debugging
-  Serial.println("LoRa Receiver");
+// void setup() {
+//   Serial.begin(115200); // Initialize serial communication for debugging
+//   Serial.println("LoRa Receiver");
 
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, i2c_Address)) {
-  Serial.println(F("SSD1306 allocation failed"));
-  for(;;); // Don't proceed, loop forever
-  }
-  Serial.println("Display Initialized OK!");
+//   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+//   if(!display.begin(SSD1306_SWITCHCAPVCC, i2c_Address)) {
+//   Serial.println(F("SSD1306 allocation failed"));
+//   for(;;); // Don't proceed, loop forever
+//   }
+//   Serial.println("Display Initialized OK!");
   
-  // Initialize LoRa module
-  LoRa.setPins(SS, RST, DI0); // Set the LoRa module's pins
+//   // Initialize LoRa module
+//   LoRa.setPins(SS, RST, DI0); // Set the LoRa module's pins
   
-  if (!LoRa.begin(915E6)) { // Or ayour desired frequency (e.g., 433MHz, 868MHz, 915MHz)
-    Serial.println("Starting LoRa failed!");
-    while (1);
-  }
-  Serial.println("LoRa Initialized OK!");
+//   if (!LoRa.begin(915E6)) { // Or ayour desired frequency (e.g., 433MHz, 868MHz, 915MHz)
+//     Serial.println("Starting LoRa failed!");
+//     while (1);
+//   }
+//   Serial.println("LoRa Initialized OK!");
 
-  display.clearDisplay();           // clear the display
-  display.setTextSize(2);          // set text size to 2
-  display.setTextColor(SSD1306_WHITE); // set text color to white
-  display.setCursor(0,0);          // set cursor to top left corner
-  display.print("Lora init SUCCESS!"); // print "Lora init OK!" on the display
-  display.display();               // update the display
-}
+//   display.clearDisplay();           // clear the display
+//   display.setTextSize(2);          // set text size to 2
+//   display.setTextColor(SSD1306_WHITE); // set text color to white
+//   display.setCursor(0,0);          // set cursor to top left corner
+//   display.print("Lora init SUCCESS!"); // print "Lora init OK!" on the display
+//   display.display();               // update the display
+// }
 
-void loop() {
-  // try to parse packet
-  int packetSize = LoRa.parsePacket(); // returns 0 if no packet available else returns size of packet
-  if (packetSize) {                     //// if packetSize > 0, it means we have a packet
-    // received a packet
-    Serial.print("Received packet '");
+// void loop() {
+//   // try to parse packet
+//   int packetSize = LoRa.parsePacket(); // returns 0 if no packet available else returns size of packet
+//   if (packetSize) {                     //// if packetSize > 0, it means we have a packet
+//     // received a packet
+//     Serial.print("Received packet '");
 
-    // read packet
-    while (LoRa.available()) {          // while there is data available in the packet
-      Serial.print((char)LoRa.read());  // read the data byte by byte
-      //store the data in a variable
-      String receivedData = LoRa.readString(); // read the data as a string
-      //display the data on the OLED display
-      display.clearDisplay();           // clear the display
-      display.setTextSize(2);          // set text size to 2
-      display.setTextColor(SSD1306_WHITE); // set text color to white
-      display.setCursor(0,0);          // set cursor to top left corner
-      display.print("Received: ");     // printa "Received: " on the display
-      display.setCursor(0,20);         // set cursor to second line
-      display.print(receivedData);     // print the received data on the display
-      display.setCursor(0,40);         // set cursor to third line
-      display.print(LoRa.packetRssi()); // print the RSSI (Received Signal Strength Indicator) on the display
-      display.print("dB");              // print "dB" on the display
-      display.display();               // update the display
-    }
+//     // read packet
+//     while (LoRa.available()) {          // while there is data available in the packet
+//       Serial.print((char)LoRa.read());  // read the data byte by byte
+//       //store the data in a variable
+//       String receivedData = LoRa.readString(); // read the data as a string
+//       //display the data on the OLED display
+//       display.clearDisplay();           // clear the display
+//       display.setTextSize(2);          // set text size to 2
+//       display.setTextColor(SSD1306_WHITE); // set text color to white
+//       display.setCursor(0,0);          // set cursor to top left corner
+//       display.print("Received: ");     // printa "Received: " on the display
+//       display.setCursor(0,20);         // set cursor to second line
+//       display.print(receivedData);     // print the received data on the display
+//       display.setCursor(0,40);         // set cursor to third line
+//       display.print(LoRa.packetRssi()); // print the RSSI (Received Signal Strength Indicator) on the display
+//       display.print("dB");              // print "dB" on the display
+//       display.display();               // update the display
+//     }
 
-    // print RSSI of packet
-    Serial.print("' with RSSI ");      // print the RSSI (Received Signal Strength Indicator) of the packet
-    Serial.println(LoRa.packetRssi());
-  }
-}
+//     // print RSSI of packet
+//     Serial.print("' with RSSI ");      // print the RSSI (Received Signal Strength Indicator) of the packet
+//     Serial.println(LoRa.packetRssi());
+//   }
+// }
--- FERD_main/src/Lora_sender.cpp
+++ FERD_main/src/Lora_sender.cpp
@@ -11,8 +11,11 @@
 // /////////////////////////////////////////////////
 
 
-// #include <SPI.h>
-// #include <LoRa.h> // Or include the appropriate library header by sandeep mistry
+// #include <SPI.h>                // Include the SPI library for SPI communication
+// #include <LoRa.h>               // Or include the appropriate library header by sandeep mistry
+// #include <Wire.h>               // Include the Wire library for I2C communication
+// #include <Adafruit_GFX.h>       // Include the Adafruit GFX library for graphics
+// #include <Adafruit_SSD1306.h>   // Include the Adafruit SSD1306 library for OLED display
 
 // int counter = 0;
  
@@ -21,10 +24,28 @@
 // #define RST 16  // LoRa module's Reset pin
 // #define DI0 11  // LoRa module's Interrupt pin (DIO0)
 
+// //setup the display
+// #define i2c_Address 0x3C //initialize with the I2C addr 0x3C Typically eBay OLED's
+// #define SCREEN_WIDTH 128 // OLED display width, in pixels
+// #define SCREEN_HEIGHT 64 // OLED display height, in pixels
+// // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// #define OLED_RESET      -1  // Reset pin # (or -1 if sharing Arduino reset pin)
+// Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
 // void setup() {
 //   Serial.begin(115200); // Initialize serial communication for debugging
 //   Serial.println("LoRa Sender");
+
+//   pinMode(2, INPUT_PULLUP);
   
+//     // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+//     if(!display.begin(SSD1306_SWITCHCAPVCC, i2c_Address)) {
+//     Serial.println(F("SSD1306 allocation failed"));
+//     for(;;); // Don't proceed, loop forever
+//     }
+//     Serial.println("Display Initialized OK!");
+
 //   // Initialize LoRa module
 //   LoRa.setPins(SS, RST, DI0); // Set the LoRa module's pins
   
@@ -33,19 +54,41 @@
 //     while (1);
 //   }
 //   Serial.println("LoRa Initialized OK!");
+
+//   display.clearDisplay();           // clear the display
+//   display.setTextSize(2);          // set text size to 2
+//   display.setTextColor(SSD1306_WHITE); // set text color to white
+//   display.setCursor(0,0);          // set cursor to top left corner
+//   display.print("Press the Button!"); // print "Lora init OK!" on the display
+//   display.display();               // update the display
+
+//   while(digitalRead(2) == HIGH);
+
+//   display.clearDisplay();           // clear the display
+//   display.setTextSize(2);          // set text size to 2
+//   display.setTextColor(SSD1306_WHITE); // set text color to white
+//   display.setCursor(0,0);          // set cursor to top left corner
+//   display.print("Data Sent!"); // print "Lora init OK!" on the display
+//   display.display();               // update the display
+//     // send packet
+//     LoRa.beginPacket();
+//     LoRa.print(" Sending Data: ");
+//     LoRa.print(counter);
+//     LoRa.endPacket();
+    
 // }
 
 // void loop() {
-//   Serial.print("Sending packet: ");
-//   Serial.println(counter);
+// //   Serial.print("Sending data: ");
+// //   Serial.println(counter);
 
-//   // send packet
-//   LoRa.beginPacket();
-//   LoRa.print(" Count: ");
-//   LoRa.print(counter);
-//   LoRa.endPacket();
+// //   // send packet
+// //   LoRa.beginPacket();
+// //   LoRa.print(" Sending Data: ");
+// //   LoRa.print(counter);
+// //   LoRa.endPacket();
 
-//   counter++;
+// //   counter++;
 
-//   delay(2000);
+// //   delay(2000);
 // }
